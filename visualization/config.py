8fee759415334dfa839ff014b3ca9a2a28b73f09
Ketan Agrawal <agrawalk@stanford.edu>
2020-10-05 19:34:58 -0400

    moved config vars to config

---
+++ visualization/config.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"Configuration values for this project."
+
+DYNAMODB_TABLE_NAME = '100-blocks-table'
+ACTIVITY_COLORS = {
+    'Sleep': 'xkcd:cobalt',
+    'Deep Work': 'xkcd:indigo',
+    'Shallow Work': 'xkcd:violet',
+    'Misc.': 'xkcd:pale mauve',
+    'Social Time': 'xkcd:dandelion',
+    'Body': 'xkcd:orangered',
+    'Utility': 'xkcd:light grey',
+}
