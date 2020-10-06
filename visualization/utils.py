71814257814d3a4996933456ea9f562bdca6ff5f
Ketan Agrawal <agrawalk@stanford.edu>
2020-10-05 20:07:35 -0400

    add requirements.txt

--- visualization/utils.py
+++ visualization/utils.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
 
 from io import BytesIO
-import requests
+# import requests
 import pandas as pd
 import boto3
 from datetime import datetime, timedelta, timezone
 
-def load_google_sheet(google_sheet_url):
-    r = requests.get(google_sheet_url)
-    data = r.content
-    df = pd.read_csv(BytesIO(data), parse_dates=['Timestamp'])
-    return df
+# def load_google_sheet(google_sheet_url):
+#     r = requests.get(google_sheet_url)
+#     data = r.content
+#     df = pd.read_csv(BytesIO(data), parse_dates=['Timestamp'])
+#     return df
 
 def load_dynamo_table(table_name, start_dt=None, end_dt=None):
     dynamodb = boto3.resource('dynamodb')
